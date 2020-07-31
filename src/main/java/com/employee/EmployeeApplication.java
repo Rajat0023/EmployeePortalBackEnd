50536065782d7ffb1d58599682e7c150ca5bfce9
Rajat0023 <rajatsingh2237@gmail.com>
2020-07-31 19:24:03 +0530

    removed redundant annotation enableAutoConfig

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -1,11 +1,9 @@
 package com.employee;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class EmployeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApplication.class, args);
